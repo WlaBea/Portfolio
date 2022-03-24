5f6b3a10336cb80444a40fc76c43dd26ddd41b06
WlaBea <92812560+WlaBea@users.noreply.github.com>
2022-03-24 17:46:12 +0100

    project + contact

--- js/main.js
+++ js/main.js
@@ -5,3 +5,8 @@ hamburger.addEventListener('click', function() {
     this.classList.toggle('is_active');
     menu.classList.toggle('show');
 });
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('show');
+    hamburger.classList.toggle('is_active');
+});
